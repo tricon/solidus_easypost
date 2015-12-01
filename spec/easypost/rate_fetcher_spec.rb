ed21e09b0bcdc5f42e50eeb1331d3ba5341571fd
Brendan Deere <brendan@stembolt.com>
2015-12-01 11:45:45 -0800

    Create a new calculator for each shipping method
    By default the preferred amount is 0, which is fine because the
    calculator will take the shippning rate amount when estimating packages

--- spec/easypost/rate_fetcher_spec.rb
+++ spec/easypost/rate_fetcher_spec.rb
@@ -14,4 +14,10 @@ describe 'Spree::Stock::Estimator customizations' do
     expect(rates.all? { |rate| rate.easy_post_shipment_id? }).to be_present
     expect(rates.all? { |rate| rate.easy_post_rate_id? }).to be_present
   end
+
+  it 'create shipping methods for the rates' do
+    order.refresh_shipment_rates
+    rates = order.shipments.first.shipping_rates
+    expect(rates.all? { |rate| rate.shipping_method.present? }).to be_truthy
+  end
 end
