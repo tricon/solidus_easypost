bbe6bb1366338dbbf03a653359304eb5d09e9877
John Hawthorn <john@stembolt.com>
2017-06-09 15:27:13 -0700

    Support Rails 5.1

--- db/migrate/20140515024440_add_easy_post_fields_to_shipping_rate.rb
+++ db/migrate/20140515024440_add_easy_post_fields_to_shipping_rate.rb
@@ -1,4 +1,4 @@
-class AddEasyPostFieldsToShippingRate < ActiveRecord::Migration
+class AddEasyPostFieldsToShippingRate < SolidusSupport::Migration[4.2]
   def change
     add_column :spree_shipping_rates, :name, :string
     add_column :spree_shipping_rates, :easy_post_shipment_id, :string
