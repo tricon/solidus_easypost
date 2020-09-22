83954206b0215513bbf09983a400cea8edd36d00
Alessandro Desantis <desa.alessandro@gmail.com>
2020-09-22 11:18:31 +0200

    Update extension to latest defaults from solidus_dev_support

--- lib/generators/solidus_easypost/install/install_generator.rb
+++ lib/generators/solidus_easypost/install/install_generator.rb
@@ -10,7 +10,7 @@ module SolidusEasypost
       end
 
       def run_migrations
-        run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask('Would you like to run the migrations now? [Y/n]')) # rubocop:disable Metrics/LineLength
+        run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask('Would you like to run the migrations now? [Y/n]')) # rubocop:disable Layout/LineLength
         if run_migrations
           run 'bin/rails db:migrate'
         else
