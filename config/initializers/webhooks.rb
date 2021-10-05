7dc0b46e0ffa122fbd99faec781b97181edaadc7
César Carruitero <ccarruitero@protonmail.com>
2021-10-05 01:23:36 -0500

    update webhook handler setup

--- config/initializers/webhooks.rb
+++ config/initializers/webhooks.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 if defined?(SolidusWebhooks)
-  SolidusWebhooks.register_webhook :easypost_trackers, ->(payload) do
+  SolidusWebhooks.config.register_webhook_handler :easypost_trackers, ->(payload) do
     SolidusEasypost::TrackerWebhookHandler.call(payload)
   end
 end
