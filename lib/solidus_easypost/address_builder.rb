e7e6cad76fa7865d99a5b7eb2cbdfc31a657890b
César Carruitero <ccarruitero@protonmail.com>
2021-11-25 12:03:47 -0500

    allow shipment and address builder with options

--- lib/solidus_easypost/address_builder.rb
+++ lib/solidus_easypost/address_builder.rb
@@ -3,7 +3,7 @@
 module SolidusEasypost
   class AddressBuilder
     class << self
-      def from_address(address)
+      def from_address(address, options = {})
         attributes = {
           street1: address.address1,
           street2: address.address2,
@@ -21,10 +21,10 @@ module SolidusEasypost
         attributes[:state] = address.state ? address.state.abbr : address.state_name
         attributes[:country] = address.country&.iso
 
-        ::EasyPost::Address.create attributes
+        ::EasyPost::Address.create attributes.merge(options)
       end
 
-      def from_stock_location(stock_location)
+      def from_stock_location(stock_location, options = {})
         attributes = {
           street1: stock_location.address1,
           street2: stock_location.address2,
@@ -38,7 +38,7 @@ module SolidusEasypost
         attributes[:state] = stock_location.state ? stock_location.state.abbr : stock_location.state_name
         attributes[:country] = stock_location.country&.iso
 
-        ::EasyPost::Address.create attributes
+        ::EasyPost::Address.create attributes.merge(options)
       end
     end
   end
--- spec/solidus_easypost/shipment_builder_spec.rb
+++ spec/solidus_easypost/shipment_builder_spec.rb
@@ -7,6 +7,22 @@ RSpec.describe SolidusEasypost::ShipmentBuilder do
 
       expect(shipment).to have_attributes(object: 'Shipment')
     end
+
+    context 'with options', vcr: { cassette_name: 'shipment_builder/from_package_with_options' } do
+      it 'allow options for shipment' do
+        message = 'an instruction'
+        shipment = described_class.from_package(create(:shipment).to_package, { handling_instructions: message })
+        expect(shipment.options).to have_attributes(handling_instructions: message.upcase)
+      end
+    end
+
+    context 'with options', vcr: { cassette_name: 'shipment_builder/from_package_with_address_options' } do
+      it 'allow address options' do
+        address_opts = { residential: false }
+        shipment = described_class.from_package(create(:shipment).to_package, { from_address_options: address_opts })
+        expect(shipment.from_address).to have_attributes(residential: false)
+      end
+    end
   end
 
   describe '.from_shipment', vcr: { cassette_name: 'shipment_builder/from_shipment' } do
